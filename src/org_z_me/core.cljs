101f2aa889c56dc983a207cc3884c8d7eeac3925
samsoul16 <cooldudeyash96@gmail.com>
2017-12-01 00:58:47 +0530

    V-0.9 Added card like styling

--- src/org_z_me/core.cljs
+++ src/org_z_me/core.cljs
@@ -17,11 +17,12 @@
 (defonce text (rn-comp "Text"))
 (defonce view (rn-comp "View"))
 (defonce image (rn-comp "Image"))
-(defonce touchable-highlight (rn-comp "TouchableHighlight"))
+(defonce touchable-highlight (rn-comp "TouchableOpacity"))
 (defonce flat-list (rn-comp "FlatList"))
 (defonce text-input (rn-comp "TextInput"))
 (defonce Alert (.-Alert ReactNative))
 (defn alert [title] (.alert Alert title))
+(defonce done-icon (js/require "./assets/images/done.png"))
 
 (defn clj->json [data] (str (.stringify js/JSON (clj->js data)) "\n"))
 (defn json->clj [line] (js->clj (.parse js/JSON line) :keywordize-keys true))
@@ -38,17 +39,22 @@
                              (dispatch [:set-key-val db-key (json->clj val)]))))
           (fn [e] (println "Error in Retrieving from store" e))))
 
+(defn delete-from-store [token-key]
+  (.catch (.then (.deleteItemAsync secure-store token-key (clj->js {}))
+                 #(println "Deleted " token-key))
+          #(println "Couldnot get, error >>" %)))
+
 (defn render-flat-list [[view navigate]]
   (fn [data]
     (let [{:keys [item index]} (js->clj data :keywordize-keys true)]
       (r/as-element (view (js->clj item) navigate)))))
 
 (defn display-task [{:keys [parent id name] :as data} navigate]
-  [touchable-highlight {:on-press (fn [_]
-                                    (println data)
-                                    (navigate "Org" {:id id}))}
-   [view {:style {:width "100%" :height 40 :margin-top 5 :backgroundColor "#ffff00"}}
-    [text name]]])
+  [view {:style {:flex-direction "row" :width "100%" :margin-bottom 10 :backgroundColor "#F1CF00" :padding 8 :z-index 2 :border-radius 2 :shadow-color "#000000" :shadow-opacity 0.3 :shadow-radius 1 :shadow-offset {:height 1 :width 0.3}}}
+   [touchable-highlight {:style {:flex 1} :on-press #(navigate "Org" {:id id})}
+    [text {:style {:font-size 17}} name]]
+   [touchable-highlight {:on-press #(println "DONE")}
+    [image {:source done-icon :style {:height 32 :width 32 :align-self "center" :margin-left 5}}]]])
 
 (defn organizer [props]
   (fn [{:keys [screenProps navigation] :as props}]
@@ -56,10 +62,9 @@
           id (:id (:params state))
           dialog? (r/atom false)
           tasks (subscribe [:get-val :tasks])
-          edited-task (r/atom "")
-          filtered (filter #(when (= (:parent %) id) %) @tasks)]
-      (fn []
-        (println filtered)
+          edited-task (r/atom "")]
+      (fn [props]
+        (println @tasks)
         [view {:flex 1 :style {:margin 10}}
          [confirm-dialog
           {:title "What's on your Mind?"
@@ -67,13 +72,13 @@
            :onTouchOutside #(reset! dialog? false)
            :positiveButton {:title "ADD"
                             :on-press (fn []
-                                        (dispatch [:set-key-val :tasks
-                                                   (conj @tasks
-                                                         {:id (if @tasks
-                                                                (inc (:id (last @tasks)))
-                                                                1)
-                                                          :parent id
-                                                          :name @edited-task})])
+                                        (dispatch-sync [:set-key-val :tasks
+                                                        (conj @tasks
+                                                              {:id (if @tasks
+                                                                     (inc (:id (last @tasks)))
+                                                                     1)
+                                                               :parent id
+                                                               :name @edited-task})])
                                         (reset! dialog? false)
                                         (save-to-store "tasks" @tasks))}
            :negativeButton {:title "CANCEL"
@@ -84,25 +89,26 @@
                              :blurOnSubmit true
                              :on-change-text #(reset! edited-task %)
                              :multiline true}]]]
-         (if (empty? filtered)
-           [text {:style {:flex 1 :text-align "center" :text-align-vertical "center" :color "#f00"}}
-            "YOU DONT HAVE ANY TASKS HERE YET!\n CLICK ADD NEW TASK AND START WRITING YOUR TASKS"]
+         (if-let [filtered (seq (filter #(when (= (:parent %) id) %) @tasks))]
            [flat-list {:keyExtractor (fn [_ i] i)
                        :data (clj->js filtered)
-                       :renderItem (render-flat-list [display-task navigate])}])
+                       :renderItem (render-flat-list [display-task navigate])}]
+           [text {:style {:flex 1 :text-align "center" :text-align-vertical "center" :color "#f00"}}
+            "YOU DONT HAVE ANY TASKS HERE YET!\n CLICK ADD NEW TASK AND START WRITING YOUR TASKS"])
          [touchable-highlight {:style {:position "absolute" :bottom 0 :width "100%"
                                        :background-color "#f00" :padding 10 :border-radius 5}
                                :on-press #(reset! dialog? true)}
           [text {:style {:text-align "center" :font-weight "bold"}} "ADD NEW TASK"]]]))))
 
-(def OrgStack (stack-navigator {:Org {:screen (stack-screen organizer {:title "Org-Z-mE"})}}
+(def OrgStack (stack-navigator {:Org {:screen (stack-screen organizer {:title "Org-Z-mE" :headerBackTitle " "})}}
                                {:initialRoute "Org" :initialRouteParams {:id 0}}))
 
 (defn app-root []
   (fn []
-    (get-from-store "tasks" :tasks)
+    ;;(delete-from-store "tasks")
     (when (= (.-NODE_ENV env) "development")
       (set! (.-REACT_NAV_LOGGING (.-env js/process)) true))
+    (get-from-store "tasks" :tasks)
     [:> OrgStack {}]))
 
 (defn init []
