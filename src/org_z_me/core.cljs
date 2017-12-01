aa0f68a1531f00c90b89153f202c6b77991cb3c3
sam16 <cooldudeyash96@gmail.com>
2017-12-01 11:58:55 +0530

    V-1.0 Of App published

--- src/org_z_me/core.cljs
+++ src/org_z_me/core.cljs
@@ -23,26 +23,26 @@
 (defonce Alert (.-Alert ReactNative))
 (defn alert [title] (.alert Alert title))
 (defonce done-icon (js/require "./assets/images/done.png"))
+(defonce colors ["#F02F1D" "#0D3D56" "#D3B53D" "#F26D21" "#1287A8" "#829356"])
 
 (defn clj->json [data] (str (.stringify js/JSON (clj->js data)) "\n"))
 (defn json->clj [line] (js->clj (.parse js/JSON line) :keywordize-keys true))
+(defn print-msg [& msg] (when (= (.-NODE_ENV env) "development") (println (apply str msg))))
 
 (defn save-to-store [token-key token-value]
-  (.catch (.then (.setItemAsync secure-store token-key (clj->json token-value) (clj->js {}))
-                 #(println "Saved " token-key))
-          #(println "Couldnot save, error >>" %)))
+  (-> (.setItemAsync secure-store token-key (clj->json token-value) (clj->js {}))
+      (.then #(print-msg "Saved " token-key))
+      (.catch #(print-msg "Couldnot save, error >>" %))))
 
 (defn get-from-store [token-key db-key]
-  (.catch (.then (.getItemAsync secure-store token-key (clj->js {}))
-                 (fn [val] (if (nil? val)
-                             (println "NOT GOT")
-                             (dispatch [:set-key-val db-key (json->clj val)]))))
-          (fn [e] (println "Error in Retrieving from store" e))))
+  (-> (.getItemAsync secure-store token-key (clj->js {}))
+      (.then  (fn [val] (when val (dispatch [:set-key-val db-key (json->clj val)]))))
+      (.catch (fn [e] (print-msg "Error in Retrieving from store" e)))))
 
 (defn delete-from-store [token-key]
-  (.catch (.then (.deleteItemAsync secure-store token-key (clj->js {}))
-                 #(println "Deleted " token-key))
-          #(println "Couldnot get, error >>" %)))
+  (-> (.deleteItemAsync secure-store token-key (clj->js {}))
+      (.then #(print-msg "Deleted " token-key))
+      (.catch #(print-msg "Couldnot get, error >>" %))))
 
 (defn render-flat-list [[view navigate]]
   (fn [data]
@@ -50,11 +50,10 @@
       (r/as-element (view (js->clj item) navigate)))))
 
 (defn display-task [{:keys [parent id name] :as data} navigate]
-  [view {:style {:flex-direction "row" :width "100%" :margin-bottom 10 :backgroundColor "#F1CF00" :padding 8 :z-index 2 :border-radius 2 :shadow-color "#000000" :shadow-opacity 0.3 :shadow-radius 1 :shadow-offset {:height 1 :width 0.3}}}
+  [view {:style {:flex-direction "row" :width "100%" :margin-bottom 10 :padding 8 :z-index 2
+                 :backgroundColor (get colors (mod parent 6)) :border-radius 2 :shadow-color "#000000" :shadow-opacity 0.3 :shadow-radius 1 :shadow-offset {:height 1 :width 0.3}}}
    [touchable-highlight {:style {:flex 1} :on-press #(navigate "Org" {:id id})}
-    [text {:style {:font-size 17}} name]]
-   [touchable-highlight {:on-press #(println "DONE")}
-    [image {:source done-icon :style {:height 32 :width 32 :align-self "center" :margin-left 5}}]]])
+    [text {:style {:font-size 17 :color "#ffffff"}} name]]])
 
 (defn organizer [props]
   (fn [{:keys [screenProps navigation] :as props}]
@@ -64,7 +63,6 @@
           tasks (subscribe [:get-val :tasks])
           edited-task (r/atom "")]
       (fn [props]
-        (println @tasks)
         [view {:flex 1 :style {:margin 10}}
          [confirm-dialog
           {:title "What's on your Mind?"
@@ -73,12 +71,12 @@
            :positiveButton {:title "ADD"
                             :on-press (fn []
                                         (dispatch-sync [:set-key-val :tasks
-                                                        (conj @tasks
-                                                              {:id (if @tasks
-                                                                     (inc (:id (last @tasks)))
-                                                                     1)
-                                                               :parent id
-                                                               :name @edited-task})])
+                                                        (into [] (conj @tasks
+                                                                       {:id (if @tasks
+                                                                              (inc (:id (last @tasks)))
+                                                                              1)
+                                                                        :parent id
+                                                                        :name @edited-task}))])
                                         (reset! dialog? false)
                                         (save-to-store "tasks" @tasks))}
            :negativeButton {:title "CANCEL"
@@ -87,6 +85,7 @@
                              :placeholder "Enter your task!"
                              :returnKeyType "done"
                              :blurOnSubmit true
+                             :underlineColorAndroid "#00000000"
                              :on-change-text #(reset! edited-task %)
                              :multiline true}]]]
          (if-let [filtered (seq (filter #(when (= (:parent %) id) %) @tasks))]
@@ -96,7 +95,7 @@
            [text {:style {:flex 1 :text-align "center" :text-align-vertical "center" :color "#f00"}}
             "YOU DONT HAVE ANY TASKS HERE YET!\n CLICK ADD NEW TASK AND START WRITING YOUR TASKS"])
          [touchable-highlight {:style {:position "absolute" :bottom 0 :width "100%"
-                                       :background-color "#f00" :padding 10 :border-radius 5}
+                                       :background-color "#FF594A" :padding 10 :border-radius 5}
                                :on-press #(reset! dialog? true)}
           [text {:style {:text-align "center" :font-weight "bold"}} "ADD NEW TASK"]]]))))
 
@@ -106,8 +105,6 @@
 (defn app-root []
   (fn []
     ;;(delete-from-store "tasks")
-    (when (= (.-NODE_ENV env) "development")
-      (set! (.-REACT_NAV_LOGGING (.-env js/process)) true))
     (get-from-store "tasks" :tasks)
     [:> OrgStack {}]))
 
